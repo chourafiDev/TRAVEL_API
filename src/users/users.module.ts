c8553758eb39df8b3bc584acb87dd0345451bf7c
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-22 19:45:05 +0100

    refactor: add cloudinary module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
   providers: [UsersService],
   exports: [UsersService],
+  imports: [CloudinaryModule],
   controllers: [UsersController],
 })
 export class UsersModule {}
