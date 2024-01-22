6a22ec069e99ca6b4e2ab4d98a5b6e160cdaf139
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-22 19:44:02 +0100

    refactor: add cloudinary

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { CloudinaryService } from './cloudinary/cloudinary.service';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule],
+  imports: [PrismaModule, AuthModule, UsersModule, CloudinaryModule, CategoriesModule],
+  providers: [CloudinaryService],
 })
 export class AppModule {}
