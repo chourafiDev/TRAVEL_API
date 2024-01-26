80a84479151de3463f3a2c0ba01af8934d36f5c1
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-26 19:44:22 +0100

    refactor: clean code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConfig } from 'src/config';
