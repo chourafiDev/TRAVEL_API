6cd1923db9768fcdc6885d27307f8372040884df
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-26 19:55:57 +0100

    refactor: add usersModule

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
