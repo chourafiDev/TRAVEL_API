5f0b88525dd11f025c663ad40f93b5ec435d339b
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-29 17:00:53 +0100

    refactor: handle create category

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -2,13 +2,42 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Injectable()
 export class CategoriesService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly cloudinaryService: CloudinaryService,
+  ) {}
 
   async create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+    console.log('createCategoryDto', createCategoryDto);
+    const { image, content } = createCategoryDto;
+
+    // Upload the image to Cloudinary
+    let imageUrl = '';
+    let imagePublicId = '';
+
+    if (image) {
+      const res = await this.cloudinaryService.uploadImage(image, 'categories');
+      imageUrl = res.secure_url;
+      imagePublicId = res.public_id;
+      console.log('res', res);
+    }
+
+    await this.prisma.category.create({
+      data: {
+        content,
+        imageUrl,
+        imagePublicId,
+      },
+    });
+
+    return {
+      statusCode: 201,
+      message: 'Category Created Successfull',
+    };
   }
 
   async findAll() {
