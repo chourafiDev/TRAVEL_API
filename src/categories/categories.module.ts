8738f365b03ef16e27459d80f20b29f50c11505b
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-29 13:15:39 +0100

    refactor: add cloudenaru module

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { UsersModule } from 'src/users/users.module';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, CloudinaryModule],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
