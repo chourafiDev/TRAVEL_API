1cc1712702054df12506477360ea23cec594546b
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-29 17:00:23 +0100

    refactor: clean code

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,25 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { v2 } from 'cloudinary';
 import { CloudinaryResponse } from './cloudinary-response';
-import toStream from 'buffer-to-stream';
 
 @Injectable()
 export class CloudinaryService {
   async uploadImage(
-    file: Express.Multer.File,
+    file: string,
     fileFolder: string,
   ): Promise<CloudinaryResponse> {
-    return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream(
-        {
-          folder: `travel/${fileFolder}`,
-        },
-        (error, result) => {
-          if (error) return reject(error);
-          resolve(result);
-        },
-      );
-      toStream(file.buffer).pipe(upload);
+    return await v2.uploader.upload(file, {
+      folder: `travel/${fileFolder}`,
     });
   }
 }
