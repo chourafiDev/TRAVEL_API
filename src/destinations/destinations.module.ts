a0b360b274598c3ca65a6d1ee47f95c4f8b5b7cc
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-02 18:18:06 +0100

    refactor: add user module

--- src/destinations/destinations.module.ts
+++ src/destinations/destinations.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DestinationsService } from './destinations.service';
 import { DestinationsController } from './destinations.controller';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [CloudinaryModule],
+  imports: [CloudinaryModule, UsersModule],
   controllers: [DestinationsController],
   providers: [DestinationsService],
 })
