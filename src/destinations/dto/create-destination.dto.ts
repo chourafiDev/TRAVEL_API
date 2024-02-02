96675e17e1af530d4571e12ce5d51b413d164479
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-02 18:08:28 +0100

    refactor: add destination

--- src/destinations/dto/create-destination.dto.ts
+++ src/destinations/dto/create-destination.dto.ts
@@ -1,1 +1,31 @@
-export class CreateDestinationDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateDestinationDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  destination: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  categoryId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  duration: number;
+
+  @IsNotEmpty()
+  @IsString({ each: true })
+  images: string[];
+}
