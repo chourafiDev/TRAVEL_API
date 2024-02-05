0a33802e91bf956f5dd00af2bc14d291804ac0e8
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-05 15:49:45 +0100

    reafctor: fix updateDto

--- src/destinations/dto/update-destination.dto.ts
+++ src/destinations/dto/update-destination.dto.ts
@@ -1,4 +1,9 @@
+import { IsOptional, IsString } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDestinationDto } from './create-destination.dto';
 
-export class UpdateDestinationDto extends PartialType(CreateDestinationDto) {}
+export class UpdateDestinationDto extends PartialType(CreateDestinationDto) {
+  @IsOptional()
+  @IsString({ each: true })
+  images?: string[];
+}
