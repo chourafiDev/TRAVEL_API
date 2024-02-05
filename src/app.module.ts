1fc55610fbff359028530ac4d83166c86a5efec6
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-05 18:32:58 +0100

    refactor: add FavoritesModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { CloudinaryService } from './cloudinary/cloudinary.service';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { CategoriesModule } from './categories/categories.module';
 import { DestinationsModule } from './destinations/destinations.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, CloudinaryModule, CategoriesModule, DestinationsModule],
+  imports: [PrismaModule, AuthModule, UsersModule, CloudinaryModule, CategoriesModule, DestinationsModule, FavoritesModule],
   providers: [CloudinaryService],
 })
 export class AppModule {}
