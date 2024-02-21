9ab0e9b4ab10879761d77ce132cf5426c69e32c7
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-21 17:52:45 +0100

    refactor: add ConfigModule and StripeModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,31 @@ import { DestinationsModule } from './destinations/destinations.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { ProfileModule } from './profile/profile.module';
 import { ReviewsModule } from './reviews/reviews.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { StripeModule } from './stripe/stripe.module';
+import { BookingModule } from './booking/booking.module';
+import configs from './config';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, CloudinaryModule, CategoriesModule, DestinationsModule, FavoritesModule, ProfileModule, ReviewsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configs],
+    }),
+    StripeModule.forRoot(process.env.STRIPE_API_KEY, {
+      apiVersion: '2023-10-16',
+    }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    CloudinaryModule,
+    CategoriesModule,
+    DestinationsModule,
+    FavoritesModule,
+    ProfileModule,
+    ReviewsModule,
+    BookingModule,
+  ],
   providers: [CloudinaryService],
 })
 export class AppModule {}
