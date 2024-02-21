d114c593c702db18444b3978612eefa3e8cb00f3
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-21 17:56:32 +0100

    refactor: handle .env variables by nest/config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,26 @@
-export const jwtConfig = {
-  secret: process.env.JWT_SECRET,
-  expired: process.env.JWT_EXPIRED,
-};
-
-export const cloudinaryConfig = {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-};
+import * as process from 'process';
+const configs = () => ({
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  GLOBAL: {
+    PORT: process.env.PORT || 3000,
+  },
+  JWT_CONFIG: {
+    SECRET: process.env.JWT_SECRET,
+    EXPIRED: process.env.JWT_EXPIRED,
+  },
+  CLOUDINARY_CONFIG: {
+    CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    API_KEY: process.env.CLOUDINARY_API_KEY,
+    API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  },
+  STRIPE_CONFIG: {
+    API_KEY: process.env.STRIPE_API_KEY,
+    webhookConfig: {
+      requestBodyProperty: 'rawBody',
+      stripeSecrets: {
+        account: process.env.STRIPE_WEBHOOK_SECRET,
+      },
+    },
+  },
+});
+export default configs;
