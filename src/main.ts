a1ac43e1c8fedfb44db0618c49f669c62b080b1d
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-21 17:55:59 +0100

    refactor: add nest/config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,9 @@ async function bootstrap() {
 
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('GLOBAL.PORT');
 
-  await app.listen(8080);
+  await app.listen(port);
 }
 bootstrap();
