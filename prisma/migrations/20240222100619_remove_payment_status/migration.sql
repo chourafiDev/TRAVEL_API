9758849a9ef66afce1b9cef27451e5cc9814e76b
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-22 12:22:20 +0100

    refactor: add migration remove_payment_status

---
+++ prisma/migrations/20240222100619_remove_payment_status/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `payment_status` on the `Bookings` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Bookings" DROP COLUMN "payment_status";
