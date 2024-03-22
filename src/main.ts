6684f1de276c037e5270104307df4a20352981e9
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-03-22 12:15:19 +0000

    refactor: config main

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,44 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import * as bodyParser from 'body-parser';
+// import { ConfigService } from '@nestjs/config';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.useGlobalPipes(
+//     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+//   );
+
+//   app.use(bodyParser.json({ limit: '50mb' }));
+//   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+//   const configService = app.get(ConfigService);
+//   const port = configService.get<number>('GLOBAL.PORT');
+
+//   await app.listen(port);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
 import { ConfigService } from '@nestjs/config';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('GLOBAL.PORT');
 
-  await app.listen(port);
+  await app.init();
+  return server;
 }
-bootstrap();
+
+module.exports = bootstrap();
